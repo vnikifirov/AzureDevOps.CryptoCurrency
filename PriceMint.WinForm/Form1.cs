87abc673bd567e1067ee4d4fe9db104e1621630c
Vadim <nikifovadim@gmail.com>
2022-05-14 22:45:22 +0300

    Added calc price of sneakers with levels by currency  which's based on sock market

--- PriceMint.WinForm/Form1.cs
+++ PriceMint.WinForm/Form1.cs
@@ -15,20 +15,106 @@ namespace PriceMint
             StartTimer();
         }
 
+        public decimal PriceSneakers(decimal Price, decimal PriceSneakers) =>  Price * PriceSneakers;
+        public decimal PriceLvlUp(decimal PriceGst,decimal PriceGmt, decimal PriceLvlUpGst, decimal PriceLvlUpGmt) => (PriceGst * PriceLvlUpGst)+(PriceGmt * PriceLvlUpGmt);
+        public decimal PriceMint(decimal PriceGst, decimal PriceGmt, decimal PriceMintGst, decimal PriceMintGmt) => (PriceGst * PriceMintGst)+(PriceGmt * PriceMintGmt);
+        public decimal PriceMintPlusLvlUp(decimal PriceMint, decimal PriceLvlUp) => (PriceMint + PriceLvlUp);
+        public decimal Profit(decimal MinPriceSneakers, decimal _priceMintPlusLvlUp) => (MinPriceSneakers - _priceMintPlusLvlUp);
+
+
+        public void ManualMintPriceGstAndGmt()
+        {
+            decimal.TryParse(gst.Text, out decimal _gst);
+            if (_gst < 2)
+            {
+                PriceMintGst.Text = "200";
+                PriceMintGmt.Text = "0";
+            }
+            else if(_gst>=2 & _gst < 3)
+            {
+                PriceMintGst.Text = "160";
+                PriceMintGmt.Text = "40";
+            }
+            else if (_gst >=3 & _gst < 4)
+            {
+                PriceMintGst.Text = "120";
+                PriceMintGmt.Text = "80";
+            }
+            else if (_gst >=4 & _gst < 8)
+            {
+                PriceMintGst.Text = "100";
+                PriceMintGmt.Text = "100";
+            }
+            else if (_gst >=8 & _gst < 10)
+            {
+                PriceMintGst.Text = "80";
+                PriceMintGmt.Text = "120";
+            }
+            else
+            {
+                PriceMintGst.Text = "40";
+                PriceMintGmt.Text = "160";
+            }
+        }
+
+        public void ProFIt()
+        {
+            decimal.TryParse(PriceSneakersDollarFive.Text, out decimal _priceSneakersDollarFive);
+            decimal.TryParse(PriceSneakersMintPlusLvlUp.Text, out decimal _priceSneakersMintPlusLvlUp);
+            ProfitLable.Text = Profit(_priceSneakersDollarFive, _priceSneakersMintPlusLvlUp).ToString();
+        }
+        public void PriceMintPlusLvlUp()
+        {
+            decimal.TryParse(PriceMintDol.Text, out decimal _priceMintDol);
+            decimal.TryParse(PriceLvlUpDol.Text, out decimal _priceLvlUpDol);
+            PriceSneakersMintPlusLvlUp.Text = PriceMintPlusLvlUp(_priceMintDol,_priceLvlUpDol).ToString();
+        }
+        public void MinPriceSneakers()
+        {
+            decimal.TryParse(Solana.Text, out decimal solanaPrice);
+            decimal.TryParse(PriceSneakersMinSolZero.Text, out decimal PriceSneakersZeroMinSolPrice);
+            PriceSneakersDollarZero.Text = PriceSneakers(solanaPrice, PriceSneakersZeroMinSolPrice).ToString();
+            decimal.TryParse(PriceSneakersMinSolFive.Text, out decimal PriceSneakersFiveMinSolPrice);
+            PriceSneakersDollarFive.Text = PriceSneakers(solanaPrice, PriceSneakersFiveMinSolPrice).ToString();
+        }
+
+        public void PriceLvlUpZeroToFive()
+        {
+            decimal.TryParse(gst.Text, out decimal gstPrice);
+            decimal.TryParse(ManualPriceLvlUpGst.Text, out decimal ManualPriceLvlUpGstText);
+            decimal.TryParse(gmt.Text, out decimal gmtPrice);
+            decimal.TryParse(ManualPriceLvlUpGmt.Text, out decimal ManualPriceLvlUpGmtText);
+            PriceLvlUpDol.Text = PriceLvlUp(gstPrice,gmtPrice,ManualPriceLvlUpGstText,ManualPriceLvlUpGmtText).ToString();
+        }
+
+        public void PriceMint()
+        {
+            decimal.TryParse(gst.Text, out decimal gstPrice);
+            decimal.TryParse(PriceMintGst.Text, out decimal _priceMintGst);
+            decimal.TryParse(gmt.Text, out decimal gmtPrice);
+            decimal.TryParse(PriceMintGmt.Text, out decimal _priceMintGmt);
+            PriceMintDol.Text = PriceLvlUp(gstPrice, gmtPrice, _priceMintGst, _priceMintGmt).ToString();
+        }
+
         private async Task DoWork()
         {
 
-            Action action = () =>
+            Action action = async () =>
             {
                 gst.Text = (await _currencyService.GetCurrencyAsync<Gst>("gst2")).ToString();
+                gmt.Text = (await _currencyService.GetCurrencyAsync<Gmt>("gmt")).ToString();
+                Solana.Text = (await _currencyService.GetCurrencyAsync<Solana>("Sol")).ToString();
+                MinPriceSneakers();
+                ManualMintPriceGstAndGmt();
+                PriceLvlUpZeroToFive();
+                PriceMint();
+                PriceMintPlusLvlUp();
+                ProFIt();
                 // other currency 
             };
 
             Invoke(action);
 
-            //SetNewGST((await _currencyService.GetCurrencyAsync<Gst>("gst2")).ToString());
-            //SetNewGMT((await _currencyService.GetCurrencyAsync<Gmt>("gmt")).ToString());
-            //SetNewSOL((await _currencyService.GetCurrencyAsync<Solana>("sol")).ToString());
         }
 
         public void StartTimer()
@@ -38,57 +124,6 @@ namespace PriceMint
             _timer.Interval = (int)TimeSpan.FromSeconds(5).TotalMilliseconds;
             _timer.Tick += async (sender, e) => await DoWork();
             _timer.Start();
-        }
-
-        private void SetNewGST(string currency)
-        {
-            // if the current thread isn't the UI thread
-            if (gst.InvokeRequired)
-            {
-                // create a delegate for this method and push it to the UI thread
-                //SetCountDelegate d = new SetCountDelegate(SetNewCounts);
-                Action action = () => { gst.Text = currency; };
-                Invoke(action);
-            }
-            else
-            {
-                // update the UI
-                gst.Text = currency;
-            }
-        }
-
-        private void SetNewGMT(string currency)
-        {
-            // if the current thread isn't the UI thread
-            if (gmt.InvokeRequired)
-            {
-                // create a delegate for this method and push it to the UI thread
-                //SetCountDelegate d = new SetCountDelegate(SetNewCounts);
-                Action action = () => { gmt.Text = currency; };
-                Invoke(action);
-            }
-            else
-            {
-                // update the UI
-                gmt.Text = currency;
-            }
-        }
-
-        private void SetNewSOL(string currency)
-        {
-            // if the current thread isn't the UI thread
-            if (gmt.InvokeRequired)
-            {
-                // create a delegate for this method and push it to the UI thread
-                //SetCountDelegate d = new SetCountDelegate(SetNewCounts);
-                Action action = () => { Solana.Text = currency; };
-                Invoke(action);
-            }
-            else
-            {
-                // update the UI
-                Solana.Text = currency;
-            }
         }
     }
 }

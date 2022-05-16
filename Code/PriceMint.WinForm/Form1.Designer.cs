9c0d61249a007c5a8be8aeb9d05e9fb15bf8704a
vnikifirov <nikifovadim@gmail.com>
2022-05-16 14:43:03 +0300

    Improved Software Architecture and how project organized

---
+++ Code/PriceMint.WinForm/Form1.Designer.cs
@@ -0,0 +1,411 @@
+﻿namespace PriceMint
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.gst = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gmt = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Solana = new System.Windows.Forms.Label();
+            this.PriceSneakers0 = new System.Windows.Forms.Label();
+            this.PriceSneakersSol = new System.Windows.Forms.Label();
+            this.PriceSneakersMinSolZero = new System.Windows.Forms.TextBox();
+            this.PriceSneakersDollarZero = new System.Windows.Forms.Label();
+            this.PriceSneakersDollarFive = new System.Windows.Forms.Label();
+            this.PriceSneakersMinSolFive = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PriceSneakers5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.ManualPriceLvlUpGst = new System.Windows.Forms.TextBox();
+            this.ManualPriceLvlUpGmt = new System.Windows.Forms.TextBox();
+            this.PriceMintGst = new System.Windows.Forms.TextBox();
+            this.PriceMintGmt = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.ProfitLable = new System.Windows.Forms.Label();
+            this.PriceSneakersMintPlusLvlUp = new System.Windows.Forms.Label();
+            this.PriceLvlUpDol = new System.Windows.Forms.Label();
+            this.PriceMintDol = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gst exchange rate$";
+            // 
+            // gst
+            // 
+            this.gst.AutoSize = true;
+            this.gst.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.gst.Location = new System.Drawing.Point(35, 24);
+            this.gst.Name = "gst";
+            this.gst.Size = new System.Drawing.Size(29, 15);
+            this.gst.TabIndex = 1;
+            this.gst.Text = "gst$";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(125, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gmt exchange rate$";
+            // 
+            // gmt
+            // 
+            this.gmt.AutoSize = true;
+            this.gmt.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.gmt.Location = new System.Drawing.Point(154, 24);
+            this.gmt.Name = "gmt";
+            this.gmt.Size = new System.Drawing.Size(35, 15);
+            this.gmt.TabIndex = 3;
+            this.gmt.Text = "gmt$";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(244, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Solana exchange rate$";
+            // 
+            // Solana
+            // 
+            this.Solana.AutoSize = true;
+            this.Solana.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Solana.Location = new System.Drawing.Point(284, 24);
+            this.Solana.Name = "Solana";
+            this.Solana.Size = new System.Drawing.Size(48, 15);
+            this.Solana.TabIndex = 5;
+            this.Solana.Text = "Solana$";
+            // 
+            // PriceSneakers0
+            // 
+            this.PriceSneakers0.AutoSize = true;
+            this.PriceSneakers0.Location = new System.Drawing.Point(12, 105);
+            this.PriceSneakers0.Name = "PriceSneakers0";
+            this.PriceSneakers0.Size = new System.Drawing.Size(99, 15);
+            this.PriceSneakers0.TabIndex = 6;
+            this.PriceSneakers0.Text = "PriceSneakers 0-0";
+            // 
+            // PriceSneakersSol
+            // 
+            this.PriceSneakersSol.AutoSize = true;
+            this.PriceSneakersSol.Location = new System.Drawing.Point(12, 120);
+            this.PriceSneakersSol.Name = "PriceSneakersSol";
+            this.PriceSneakersSol.Size = new System.Drawing.Size(81, 15);
+            this.PriceSneakersSol.TabIndex = 9;
+            this.PriceSneakersSol.Text = "Solana       $$$";
+            // 
+            // PriceSneakersMinSolZero
+            // 
+            this.PriceSneakersMinSolZero.Location = new System.Drawing.Point(12, 138);
+            this.PriceSneakersMinSolZero.Name = "PriceSneakersMinSolZero";
+            this.PriceSneakersMinSolZero.Size = new System.Drawing.Size(38, 23);
+            this.PriceSneakersMinSolZero.TabIndex = 10;
+            this.PriceSneakersMinSolZero.Text = "10";
+            // 
+            // PriceSneakersDollarZero
+            // 
+            this.PriceSneakersDollarZero.AutoSize = true;
+            this.PriceSneakersDollarZero.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.PriceSneakersDollarZero.Location = new System.Drawing.Point(67, 141);
+            this.PriceSneakersDollarZero.Name = "PriceSneakersDollarZero";
+            this.PriceSneakersDollarZero.Size = new System.Drawing.Size(25, 15);
+            this.PriceSneakersDollarZero.TabIndex = 11;
+            this.PriceSneakersDollarZero.Text = "$$$";
+            // 
+            // PriceSneakersDollarFive
+            // 
+            this.PriceSneakersDollarFive.AutoSize = true;
+            this.PriceSneakersDollarFive.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.PriceSneakersDollarFive.Location = new System.Drawing.Point(180, 141);
+            this.PriceSneakersDollarFive.Name = "PriceSneakersDollarFive";
+            this.PriceSneakersDollarFive.Size = new System.Drawing.Size(25, 15);
+            this.PriceSneakersDollarFive.TabIndex = 15;
+            this.PriceSneakersDollarFive.Text = "$$$";
+            // 
+            // PriceSneakersMinSolFive
+            // 
+            this.PriceSneakersMinSolFive.Location = new System.Drawing.Point(125, 138);
+            this.PriceSneakersMinSolFive.Name = "PriceSneakersMinSolFive";
+            this.PriceSneakersMinSolFive.Size = new System.Drawing.Size(38, 23);
+            this.PriceSneakersMinSolFive.TabIndex = 14;
+            this.PriceSneakersMinSolFive.Text = "10";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(125, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 15);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Solana       $$$";
+            // 
+            // PriceSneakers5
+            // 
+            this.PriceSneakers5.AutoSize = true;
+            this.PriceSneakers5.Location = new System.Drawing.Point(125, 105);
+            this.PriceSneakers5.Name = "PriceSneakers5";
+            this.PriceSneakers5.Size = new System.Drawing.Size(99, 15);
+            this.PriceSneakers5.TabIndex = 12;
+            this.PriceSneakers5.Text = "PriceSneakers 5-2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 15);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Price 0-5 lvl up ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(125, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 15);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Price mint $$$";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 197);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 15);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "GST";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(65, 197);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 15);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "GMT";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(178, 197);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 15);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "GMT";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(125, 197);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(27, 15);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "GST";
+            // 
+            // ManualPriceLvlUpGst
+            // 
+            this.ManualPriceLvlUpGst.Location = new System.Drawing.Point(12, 215);
+            this.ManualPriceLvlUpGst.Name = "ManualPriceLvlUpGst";
+            this.ManualPriceLvlUpGst.Size = new System.Drawing.Size(38, 23);
+            this.ManualPriceLvlUpGst.TabIndex = 22;
+            this.ManualPriceLvlUpGst.Text = "20";
+            // 
+            // ManualPriceLvlUpGmt
+            // 
+            this.ManualPriceLvlUpGmt.Location = new System.Drawing.Point(54, 215);
+            this.ManualPriceLvlUpGmt.Name = "ManualPriceLvlUpGmt";
+            this.ManualPriceLvlUpGmt.Size = new System.Drawing.Size(38, 23);
+            this.ManualPriceLvlUpGmt.TabIndex = 23;
+            this.ManualPriceLvlUpGmt.Text = "10";
+            // 
+            // PriceMintGst
+            // 
+            this.PriceMintGst.Location = new System.Drawing.Point(125, 215);
+            this.PriceMintGst.Name = "PriceMintGst";
+            this.PriceMintGst.Size = new System.Drawing.Size(38, 23);
+            this.PriceMintGst.TabIndex = 24;
+            this.PriceMintGst.Text = "0";
+            // 
+            // PriceMintGmt
+            // 
+            this.PriceMintGmt.Location = new System.Drawing.Point(167, 215);
+            this.PriceMintGmt.Name = "PriceMintGmt";
+            this.PriceMintGmt.Size = new System.Drawing.Size(38, 23);
+            this.PriceMintGmt.TabIndex = 25;
+            this.PriceMintGmt.Text = "0";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(244, 105);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(94, 15);
+            this.label11.TabIndex = 26;
+            this.label11.Text = "Price mint+lvl ip";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(244, 182);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(75, 15);
+            this.label12.TabIndex = 27;
+            this.label12.Text = "Profit       $$$";
+            // 
+            // ProfitLable
+            // 
+            this.ProfitLable.AutoSize = true;
+            this.ProfitLable.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ProfitLable.Location = new System.Drawing.Point(284, 218);
+            this.ProfitLable.Name = "ProfitLable";
+            this.ProfitLable.Size = new System.Drawing.Size(25, 15);
+            this.ProfitLable.TabIndex = 28;
+            this.ProfitLable.Text = "$$$";
+            // 
+            // PriceSneakersMintPlusLvlUp
+            // 
+            this.PriceSneakersMintPlusLvlUp.AutoSize = true;
+            this.PriceSneakersMintPlusLvlUp.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.PriceSneakersMintPlusLvlUp.Location = new System.Drawing.Point(284, 141);
+            this.PriceSneakersMintPlusLvlUp.Name = "PriceSneakersMintPlusLvlUp";
+            this.PriceSneakersMintPlusLvlUp.Size = new System.Drawing.Size(25, 15);
+            this.PriceSneakersMintPlusLvlUp.TabIndex = 29;
+            this.PriceSneakersMintPlusLvlUp.Text = "$$$";
+            // 
+            // PriceLvlUpDol
+            // 
+            this.PriceLvlUpDol.AutoSize = true;
+            this.PriceLvlUpDol.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.PriceLvlUpDol.Location = new System.Drawing.Point(20, 257);
+            this.PriceLvlUpDol.Name = "PriceLvlUpDol";
+            this.PriceLvlUpDol.Size = new System.Drawing.Size(25, 15);
+            this.PriceLvlUpDol.TabIndex = 30;
+            this.PriceLvlUpDol.Text = "$$$";
+            // 
+            // PriceMintDol
+            // 
+            this.PriceMintDol.AutoSize = true;
+            this.PriceMintDol.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.PriceMintDol.Location = new System.Drawing.Point(141, 257);
+            this.PriceMintDol.Name = "PriceMintDol";
+            this.PriceMintDol.Size = new System.Drawing.Size(25, 15);
+            this.PriceMintDol.TabIndex = 31;
+            this.PriceMintDol.Text = "$$$";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 368);
+            this.Controls.Add(this.PriceMintDol);
+            this.Controls.Add(this.PriceLvlUpDol);
+            this.Controls.Add(this.PriceSneakersMintPlusLvlUp);
+            this.Controls.Add(this.ProfitLable);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.PriceMintGmt);
+            this.Controls.Add(this.PriceMintGst);
+            this.Controls.Add(this.ManualPriceLvlUpGmt);
+            this.Controls.Add(this.ManualPriceLvlUpGst);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PriceSneakersDollarFive);
+            this.Controls.Add(this.PriceSneakersMinSolFive);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.PriceSneakers5);
+            this.Controls.Add(this.PriceSneakersDollarZero);
+            this.Controls.Add(this.PriceSneakersMinSolZero);
+            this.Controls.Add(this.PriceSneakersSol);
+            this.Controls.Add(this.PriceSneakers0);
+            this.Controls.Add(this.Solana);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.gmt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.gst);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label gst;
+        private Label label3;
+        private Label gmt;
+        private Label label5;
+        private Label Solana;
+        private Label PriceSneakers0;
+        private Label PriceSneakersSol;
+        private TextBox PriceSneakersMinSolZero;
+        private Label PriceSneakersDollarZero;
+        private Label PriceSneakersDollarFive;
+        private TextBox PriceSneakersMinSolFive;
+        private Label label4;
+        private Label PriceSneakers5;
+        private Label label2;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private TextBox ManualPriceLvlUpGst;
+        private TextBox ManualPriceLvlUpGmt;
+        private TextBox PriceMintGst;
+        private TextBox PriceMintGmt;
+        private Label label11;
+        private Label label12;
+        private Label ProfitLable;
+        private Label PriceSneakersMintPlusLvlUp;
+        private Label PriceLvlUpDol;
+        private Label PriceMintDol;
+    }
+}
---
+++ Code/PriceMint.WinForm/Form1.cs
@@ -0,0 +1,129 @@
+﻿using Currency.Business.Services.Implementations;
+using Currency.Business.Models;
+
+namespace PriceMint
+{
+    public partial class Form1 : Form
+    {
+        private CurrencyService _currencyService;
+        private System.Windows.Forms.Timer _timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _currencyService = new CurrencyService("coincodex.com", "/api/coincodex/get_coin/");
+            StartTimer();
+        }
+
+        public decimal PriceSneakers(decimal Price, decimal PriceSneakers) =>  Price * PriceSneakers;
+        public decimal PriceLvlUp(decimal PriceGst,decimal PriceGmt, decimal PriceLvlUpGst, decimal PriceLvlUpGmt) => (PriceGst * PriceLvlUpGst)+(PriceGmt * PriceLvlUpGmt);
+        public decimal PriceMint(decimal PriceGst, decimal PriceGmt, decimal PriceMintGst, decimal PriceMintGmt) => (PriceGst * PriceMintGst)+(PriceGmt * PriceMintGmt);
+        public decimal PriceMintPlusLvlUp(decimal PriceMint, decimal PriceLvlUp) => (PriceMint + PriceLvlUp);
+        public decimal Profit(decimal MinPriceSneakers, decimal _priceMintPlusLvlUp) => (MinPriceSneakers - _priceMintPlusLvlUp);
+
+
+        public void ManualMintPriceGstAndGmt()
+        {
+            decimal.TryParse(gst.Text, out decimal _gst);
+            if (_gst < 2)
+            {
+                PriceMintGst.Text = "200";
+                PriceMintGmt.Text = "0";
+            }
+            else if(_gst>=2 & _gst < 3)
+            {
+                PriceMintGst.Text = "160";
+                PriceMintGmt.Text = "40";
+            }
+            else if (_gst >=3 & _gst < 4)
+            {
+                PriceMintGst.Text = "120";
+                PriceMintGmt.Text = "80";
+            }
+            else if (_gst >=4 & _gst < 8)
+            {
+                PriceMintGst.Text = "100";
+                PriceMintGmt.Text = "100";
+            }
+            else if (_gst >=8 & _gst < 10)
+            {
+                PriceMintGst.Text = "80";
+                PriceMintGmt.Text = "120";
+            }
+            else
+            {
+                PriceMintGst.Text = "40";
+                PriceMintGmt.Text = "160";
+            }
+        }
+
+        public void ProFIt()
+        {
+            decimal.TryParse(PriceSneakersDollarFive.Text, out decimal _priceSneakersDollarFive);
+            decimal.TryParse(PriceSneakersMintPlusLvlUp.Text, out decimal _priceSneakersMintPlusLvlUp);
+            ProfitLable.Text = Profit(_priceSneakersDollarFive, _priceSneakersMintPlusLvlUp).ToString();
+        }
+        public void PriceMintPlusLvlUp()
+        {
+            decimal.TryParse(PriceMintDol.Text, out decimal _priceMintDol);
+            decimal.TryParse(PriceLvlUpDol.Text, out decimal _priceLvlUpDol);
+            PriceSneakersMintPlusLvlUp.Text = PriceMintPlusLvlUp(_priceMintDol,_priceLvlUpDol).ToString();
+        }
+        public void MinPriceSneakers()
+        {
+            decimal.TryParse(Solana.Text, out decimal solanaPrice);
+            decimal.TryParse(PriceSneakersMinSolZero.Text, out decimal PriceSneakersZeroMinSolPrice);
+            PriceSneakersDollarZero.Text = PriceSneakers(solanaPrice, PriceSneakersZeroMinSolPrice).ToString();
+            decimal.TryParse(PriceSneakersMinSolFive.Text, out decimal PriceSneakersFiveMinSolPrice);
+            PriceSneakersDollarFive.Text = PriceSneakers(solanaPrice, PriceSneakersFiveMinSolPrice).ToString();
+        }
+
+        public void PriceLvlUpZeroToFive()
+        {
+            decimal.TryParse(gst.Text, out decimal gstPrice);
+            decimal.TryParse(ManualPriceLvlUpGst.Text, out decimal ManualPriceLvlUpGstText);
+            decimal.TryParse(gmt.Text, out decimal gmtPrice);
+            decimal.TryParse(ManualPriceLvlUpGmt.Text, out decimal ManualPriceLvlUpGmtText);
+            PriceLvlUpDol.Text = PriceLvlUp(gstPrice,gmtPrice,ManualPriceLvlUpGstText,ManualPriceLvlUpGmtText).ToString();
+        }
+
+        public void PriceMint()
+        {
+            decimal.TryParse(gst.Text, out decimal gstPrice);
+            decimal.TryParse(PriceMintGst.Text, out decimal _priceMintGst);
+            decimal.TryParse(gmt.Text, out decimal gmtPrice);
+            decimal.TryParse(PriceMintGmt.Text, out decimal _priceMintGmt);
+            PriceMintDol.Text = PriceLvlUp(gstPrice, gmtPrice, _priceMintGst, _priceMintGmt).ToString();
+        }
+
+        private async Task DoWork()
+        {
+
+            Action action = async () =>
+            {
+                gst.Text = (await _currencyService.GetCurrencyAsync<Gst>("gst2")).ToString();
+                gmt.Text = (await _currencyService.GetCurrencyAsync<Gmt>("gmt")).ToString();
+                Solana.Text = (await _currencyService.GetCurrencyAsync<Solana>("Sol")).ToString();
+                MinPriceSneakers();
+                ManualMintPriceGstAndGmt();
+                PriceLvlUpZeroToFive();
+                PriceMint();
+                PriceMintPlusLvlUp();
+                ProFIt();
+                // other currency 
+            };
+
+            Invoke(action);
+
+        }
+
+        public void StartTimer()
+        {
+            // _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+            _timer = new System.Windows.Forms.Timer();
+            _timer.Interval = (int)TimeSpan.FromSeconds(5).TotalMilliseconds;
+            _timer.Tick += async (sender, e) => await DoWork();
+            _timer.Start();
+        }
+    }
+}
